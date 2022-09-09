73891bf9f13fbefd98492c7f3e683caf5dcd46ee
AldomarAssolin <assolinaldomar@gmail.com>
2022-09-09 17:41:33 -0300

    page welcome - wait
    config. page welcome site

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+import curriculo from './pdf/curriculo_22.pdf'
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +11,11 @@ function App() {
       <div className="message-site">
         <p>Estamos atualizando o site!</p>
         <p>Em breve estaremos no ar novamente.</p>
+
+        <a className="btn bg-info btn-outline-dark m-auto" href={curriculo}>
+          <i className="fa fa-download mx-2" /> Currículo em PDF
+        </a>
+
       </div>
     </div>
   );
