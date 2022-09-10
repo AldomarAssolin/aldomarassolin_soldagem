d0540a635cab71bb08e1010a31bcc24e12f0b0e1
AldomarAssolin <assolinaldomar@gmail.com>
2022-09-09 21:03:54 -0300

    Page Welcome
    Create the page single

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/logos/logo192.png';
 import './App.css';
 
 import curriculo from './pdf/curriculo_22.pdf'
@@ -6,16 +6,20 @@ import curriculo from './pdf/curriculo_22.pdf'
 function App() {
   return (
     <div className="App">
-      <h1>Aldomar Assolin</h1>
-      <h3>Soldador</h3>
-      <div className="message-site">
-        <p>Estamos atualizando o site!</p>
-        <p>Em breve estaremos no ar novamente.</p>
-
-        <a className="btn" href={curriculo} target="_blank" rel="noreferrer">
+      <header className='App-header'>
+      <div className="App-logo">
+        <img src={logo} alt="Logomarca" />
+      </div>
+        <h1>Aldomar Assolin</h1>
+        <h3>Soldador</h3>
+        <a className="btn App-link" href={curriculo} target="_blank" rel="noreferrer">
           <i className="fa fa-download mx-2" /> Currículo em PDF
         </a>
-
+      </header>
+      
+      <div className="message-site">
+        <h2>Estamos atualizando o site!</h2>
+        <h2>Em breve estaremos no ar novamente.</h2>
       </div>
     </div>
   );
